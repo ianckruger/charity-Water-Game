ce9fddb470e25f4976365be93dba5c549f3dc141
ianckruger <ian.kruger.2@gmail.com>
2025-10-12 19:09:54 -0400

    updated control layouts from official documentation

--- script.js
+++ script.js
@@ -20,34 +20,51 @@ camera.position.z = 5;
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableZoom = true;
-controls.enablePan = true;
+controls.enablePan = false;
+controls.enableDamping = true;
+// --> Damping is an automatic x and y dragging feature and it adds momentum
+// --> it lets it float for a little after dragging to keep it realistic
+controls.dampingFactor = 0.05;
+controls.minDistance = 3;
+controls.maxDistance = 10;  // adding a min and max zoom too 
 
 function animate() {
+    // removing the spin untill i figure out a way to stop it spinning after its been interacted with.
     requestAnimationFrame(animate);
-    globe.rotation.y += 0.002; // this is the spin speed
     controls.update();
     renderer.render(scene, camera);
 }
 animate();
 
-let isDragging = false;
-let prevX = 0;
-let prevY = 0;
+// this will handle all window resizing, incase of mobile phones or ipads, etc.
+window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+})
 
-document.addEventListener("mousedown", e => { 
-    isDragging = true; 
-    prevX = e.clientX;
-    prevY = e.clientY; });
 
-document.addEventListener("mouseup", () => { isDragging = false; });
-document.addEventListener("mousemove", e => {
-  if (isDragging) {
-    const delta = e.clientX - prevX;
-    globe.rotation.y += delta * 0.005;
-    prevX = e.clientX;
 
-    const delta2 = e.clientY - prevY;
-    globe.rotation.x += delta2 * 0.005;
-    prevY = e.clientY;
-  }
-});
+// Keeping old code but commented out, i will use this as a "loading" icon
+
+// let isDragging = false;
+// let prevX = 0;
+// let prevY = 0;
+
+// document.addEventListener("mousedown", e => { 
+//     isDragging = true; 
+//     prevX = e.clientX;
+//     prevY = e.clientY; });
+
+// document.addEventListener("mouseup", () => { isDragging = false; });
+// document.addEventListener("mousemove", e => {
+//   if (isDragging) {
+//     const delta = e.clientX - prevX;
+//     globe.rotation.y += delta * 0.005;
+//     prevX = e.clientX;
+
+//     const delta2 = e.clientY - prevY;
+//     globe.rotation.x += delta2 * 0.005;
+//     prevY = e.clientY;
+//   }
+// });
